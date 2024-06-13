87ce15414e4a7d0a8730cb628625cf073c42adc2
julio kania <julio.kania@gmail.com>
2024-06-13 15:24:00 -0300

    table sort finishedn

--- src/components/appTable/AppTable.jsx
+++ src/components/appTable/AppTable.jsx
@@ -1,23 +1,82 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './AppTable.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome'
+import { faSortDown, faSortUp, faSquare } from '@fortawesome/free-solid-svg-icons'
 
-function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
-    headers = headers ? [...headers] : [];
-    rows = rows ? [...rows] : [];
+export const TABLE_SORT_ORDER = {
+    ASC: 'TABLE_SORT_ORDER:ASC',
+    DESC: 'TABLE_SORT_ORDER:DESC',
+}
 
-    if(isMultiRowSelectable){
-        headers.unshift({key:'', label:'selector',align:'center', width:'5rem', isKey:false});
-        rows.forEach(row=>{
-            row.data.unshift(false) // add if checked or not the row
-        });
+export const TABLE_DATA_TYPE = {
+    NUMBER: 'TABLE_DATA_TYPE:NUMBER',
+    STRING: 'TABLE_DATA_TYPE:STRING',
+    DATE: 'TABLE_DATA_TYPE:DATE',
+    BOOLEAN: 'TABLE_DATA_TYPE:BOOLEAN',
+}
+
+const addCheckbox = (headers, rows)=>{
+    headers.unshift({key:'', label:'selector',align:'center', width:'5rem', isKey:false});
+    rows.forEach(row=>{
+        row.data.unshift(false) // add if checked or not the row
+    });
+}
+
+const castToTableType = (value='0',dataType=TABLE_DATA_TYPE.STRING  )=>{
+    switch(dataType){
+        case TABLE_DATA_TYPE.STRING:{
+            return value.toString().toUpperCase();
+            break;
+        }
+        case TABLE_DATA_TYPE.NUMBER:{
+            return Number(value);
+            break;
+        }
+        case TABLE_DATA_TYPE.BOOLEAN:{
+            return value? true : false;
+            break;
+        }
+        case TABLE_DATA_TYPE.DATE:{
+            return new Date(value);
+            break;
+        }
+        default: {
+            return value.toString().toUpperCase();
+            break;
+        } 
     }
-    const styleTrWidth =  headers.map(h=>{return h.width}).join(' '); 
-    const dataKeyIndex = headers.findIndex((e)=>{return e.isKey});
+}
+
+const sortRows = (rows, headers, sortColumnKey, sortColumnOrder )=>{
+    const index = headers.findIndex((header)=>{return header.key.toString().toUpperCase() == sortColumnKey.toString().toUpperCase()})
+    rows.sort((rowA, rowB) => {
+        if(castToTableType(rowA.data[index], headers[index].dataType) >= castToTableType(rowB.data[index], headers[index].dataType)){
+            return sortColumnOrder === TABLE_SORT_ORDER.ASC ? 1 : -1
+        } else {
+            return sortColumnOrder === TABLE_SORT_ORDER.ASC ? -1 : 1
+        }
+    });
+    console.log('sortRows()',{index, rows, headers, sortColumnKey, sortColumnOrder})
+    return rows
+}
+
+function AppTable({isLoading, title, headers, rows,isMultiRowSelectable, defaultSortColumnKey, defaultSortColumnOrder }) {
+    const headersCopy = JSON.parse(JSON.stringify(headers));
+    const rowsCopy = JSON.parse(JSON.stringify(rows));
+
+    if(isMultiRowSelectable) addCheckbox(headersCopy, rowsCopy);
+
+    const styleTrWidth =  headersCopy.map(h=>{return h.width}).join(' '); 
+    const dataKeyIndex = headersCopy.findIndex((e)=>{return e.isKey});
+
+    const [rowsState,setRowsState] = useState(sortRows(rowsCopy, headersCopy,defaultSortColumnKey, defaultSortColumnOrder));
+    const [headersState,setHeadersState] = useState(headersCopy);
 
     const [isTableLoading,setIsTableLoading] = useState(isLoading);
-    const [rowsStateData,setRowsStateData] = useState(rows);
     const [allRowsSelected,setAllRowsSelected] = useState(false);
+    const [sortColumnKey,setSortColumnKey] = useState(defaultSortColumnKey);
+    const [sortColumnOrder,setSortColumnOrder] = useState(defaultSortColumnOrder);
+
     const isFirstRender = useRef(true);  // Usamos useRef para mantener la bandera de la primera renderización
 
 
@@ -26,22 +85,22 @@ function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
             isFirstRender.current = false; // Cambiamos la bandera después de la primera renderización
             return; // No ejecutamos el efecto en la primera renderización
         } 
-        console.log('useEffect(),[rowsStateData]');
+        console.log('useEffect() called')
         let allSelected = false;
         if(isMultiRowSelectable){
-            allSelected = rowsStateData.reduce((valorAnterior, valorActual, indice, vector)=>{
+            allSelected = rowsState.reduce((valorAnterior, valorActual, indice, vector)=>{
                 return valorAnterior && valorActual.data[0]
             },true)
-            console.log(`allSelected ${allSelected}`)
             setAllRowsSelected(allSelected)
         }
         return () => {};
-    },[rowsStateData])
+    },[null,rowsState])
 
     const onChangeSelectAll = (e)=>{
-        console.log('F2: onChangeSelectAll()')
+        
+        console.log('onChangeSelectAll() called ')
         let newValue = e.target.checked;
-        setRowsStateData((currentRowStateData)=>{
+        setRowsState((currentRowStateData)=>{
             let newData = currentRowStateData.map(row=>{
                 return {...row, data:row.data.map((e,i)=>{return i==0? newValue : e}) }
             })
@@ -49,10 +108,10 @@ function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
         })
     }
     const onChangeSelectOne = (e)=>{
-        console.log('F1: onChangeSelectOne()')
+        console.log('onChangeSelectOne() called ')
         let newValue = e.target.checked;
         let dataId = e.target.parentNode.parentNode.dataset.id 
-        setRowsStateData((currentRowStateData)=>{
+        setRowsState((currentRowStateData)=>{
             let selectedElementIndex = currentRowStateData.findIndex(row=>{return row.key == dataId;});
             let newData = [...currentRowStateData]
             newData[selectedElementIndex].data[0] = newValue;
@@ -60,19 +119,19 @@ function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
         });
     }
 
-    console.log('rendering...')
+    const onSortColumnMouseDown = (e) => {
+        console.log('onSortColumnMouseDown() called ',{target: e.target})
+        const newSortKey = e.target.parentNode.dataset.key;
+        const newSortOrder = sortColumnOrder === TABLE_SORT_ORDER.ASC ? TABLE_SORT_ORDER.DESC : TABLE_SORT_ORDER.ASC;
+        setSortColumnKey(newSortKey);
+        setSortColumnOrder(newSortOrder);
+        sortRows(rowsState, headersState, newSortKey,newSortOrder);
+        setRowsState((current)=>{
+            return current//sortRows(rowsState, headersState, newSortKey,newSortOrder);
+        })
+    };
 
-
-
-
-
-
-
-
-
-
-
-
+    console.log('rendering',{headersState,rowsState,sortColumnKey, sortColumnOrder})
     return (
         <div className='app-table'>
         {
@@ -83,17 +142,24 @@ function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
                 <table>
                     <thead>
                         <tr style={{gridTemplateColumns:styleTrWidth}}>
-                            {headers.map((h,i)=>{
-                                if(i==0 && isMultiRowSelectable){
-                                    return <th key={h.key || i}><input type='checkbox' checked={allRowsSelected} onChange={onChangeSelectAll}></input> </th>
-                                }
-                                return <th key={h.key || i}>{h.label}</th>
+                            {headersState.map((h,i)=>{
+                                return <th key={h.key || i} data-key={h.key || i}>
+                                    {i==0 && isMultiRowSelectable ? 
+                                        <input type='checkbox' checked={allRowsSelected} onChange={onChangeSelectAll} /> 
+                                        : 
+                                        <button type='button' className={h.key != sortColumnKey? 'disabled' : null} onClick={onSortColumnMouseDown}>
+                                            <span htmlFor="">{h.label}</span> 
+                                            <FontAwesomeIcon icon={sortColumnOrder===TABLE_SORT_ORDER.ASC ? faSortUp : faSortDown}/>
+                                        </button>
+                                    }
+                                </th>
+
                             })}
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            rowsStateData.map((row,rowIndex)=>{
+                            rowsState.map((row,rowIndex)=>{
                                 return <tr key={row.key || rowIndex } style={{gridTemplateColumns:styleTrWidth}} data-id={row.data[dataKeyIndex]}>
                                 {
                                     row.data.map((data, dataIndex)=>{
@@ -103,14 +169,14 @@ function AppTable({isLoading, title, headers, rows,isMultiRowSelectable }) {
                                             </td>
                                         }
 
-                                        if(headers[dataIndex].isAction){
+                                        if(headersState[dataIndex].isAction){
                                             return <td key={dataIndex} data-type="action"> 
                                                 {
-                                                    <button type="button" disabled={!data}><FontAwesomeIcon icon={headers[dataIndex].icon} /></button>
+                                                    <button type="button" disabled={!data}><FontAwesomeIcon icon={headersState[dataIndex].icon} /></button>
                                                 }
                                             </td>
                                         } 
-                                        return <td key={dataIndex} style={{justifyContent:headers[dataIndex]?.align || 'center'}}> {data}</td>
+                                        return <td key={dataIndex} style={{justifyContent:headersCopy[dataIndex]?.align || 'center'}}> {data}</td>
                                     })
                                 }</tr>
                             })
--- src/pages/application/pages/tablas/Tablas.jsx
+++ src/pages/application/pages/tablas/Tablas.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AppTable from '../../../../components/appTable/AppTable';
+import AppTable, {TABLE_SORT_ORDER, TABLE_DATA_TYPE} from '../../../../components/appTable/AppTable';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 import './Tablas.css'
 
@@ -7,14 +7,16 @@ function Tablas() {
   const IS_LOADING = false;
   const IS_MULTI_ROW_SELECTABLE = true;
   const TITLE = 'Datos del Cliente';
+  const DEFAULT_SORT_COLUMN_KEY = 'FIRSTNAME';
+  const DEFAULT_SORT_ORDER = TABLE_SORT_ORDER.DESC;
   const HEADERS = [
-    {key:'ID',          label:'Id',       isKey:true, align:'center', width:'5rem', isAction:false},
-    {key:'FIRSTNAME',   label:'Nombre',   isKey:false, align:'left',   width:'1fr', isAction:false},
-    {key:'FAMILYNAME',  label:'Apellido', isKey:false, align:'left',   width:'1fr',  isAction:false},
-    {key:'AGE',         label:'Edad',     isKey:false, align:'center', width:'5rem', isAction:false},
-    {key:'ACTION_1',    label:'A1',       isKey:false, align:'center', width:'5rem', isAction:true, icon:faEllipsisVertical },
-    {key:'ACTION_2',    label:'A2',       isKey:false, align:'center', width:'5rem', isAction:true, icon:faEllipsisVertical },
-    {key:'ACTION_3',    label:'A3',       isKey:false, align:'center', width:'5rem', isAction:true, icon:faEllipsisVertical }
+    {key:'ID',          label:'Id',       isKey:true, align:'center', width:'5rem',  isAction:false,  icon:undefined,           sortable:true, dataType:TABLE_DATA_TYPE.NUMBER},
+    {key:'FIRSTNAME',   label:'Nombre',   isKey:false, align:'left',   width:'1fr',  isAction:false,  icon:undefined,           sortable:true, dataType:TABLE_DATA_TYPE.STRING},
+    {key:'FAMILYNAME',  label:'Apellido', isKey:false, align:'left',   width:'1fr',  isAction:false,  icon:undefined,           sortable:true, dataType:TABLE_DATA_TYPE.STRING},
+    {key:'AGE',         label:'Edad',     isKey:false, align:'center', width:'5rem', isAction:false,  icon:undefined,           sortable:true, dataType:TABLE_DATA_TYPE.NUMBER},
+    {key:'ACTION_1',    label:'A1',       isKey:false, align:'center', width:'5rem', isAction:true,   icon:faEllipsisVertical,  sortable:true, dataType:TABLE_DATA_TYPE.BOOLEAN},
+    {key:'ACTION_2',    label:'A2',       isKey:false, align:'center', width:'5rem', isAction:true,   icon:faEllipsisVertical,  sortable:true, dataType:TABLE_DATA_TYPE.BOOLEAN},
+    {key:'ACTION_3',    label:'A3',       isKey:false, align:'center', width:'5rem', isAction:true,   icon:faEllipsisVertical,  sortable:true, dataType:TABLE_DATA_TYPE.BOOLEAN}
   ]
   const ROWS = [
     {key:1,   data:['1',  'Julio',        'Kania',  '37', true,   true,   true]},
@@ -29,9 +31,11 @@ function Tablas() {
     {key:10,  data:['10', 'Pedro 2',      'Kania',  '50', true,   true,   false]}
   ]
 
+
+
   return (
     <div className='tablas-table-container'>
-      <AppTable isLoading={IS_LOADING} isMultiRowSelectable={IS_MULTI_ROW_SELECTABLE} title={TITLE} headers={HEADERS} rows={ROWS} />
+      <AppTable isLoading={IS_LOADING} isMultiRowSelectable={IS_MULTI_ROW_SELECTABLE} title={TITLE} headers={HEADERS} rows={ROWS} defaultSortColumnKey={DEFAULT_SORT_COLUMN_KEY} defaultSortColumnOrder={DEFAULT_SORT_ORDER} />
     </div>
   )
 }
